9362ff20edb5291201ae081f9690c12831412eea
hep656 <hep656@usask.ca>
2021-05-06 14:26:31 -0600

    guesstheflag

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Heesoo Park on 2021-05-06.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
